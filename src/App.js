89bab1f1e65bb732e8b20d781a0927db10a5c6c9
imtiyazhasankhan <imtiyaz.india@outlook.com>
2022-07-12 20:43:04 +0530

    imporoved default width and added not function search bar

--- src/App.js
+++ src/App.js
@@ -12,15 +12,40 @@ import {
 } from "react-router-dom";
 import Weather from './components/Weather';
 
-
 export default class App extends Component {
+  location_ = []
+  currentUserLocation = ""
+  feelsLike = "Chill"
 
   constructor() {
     super()
     this.state = {
       category: "general",
-      key: 'general'
+      key: 'general',
+      location: this.location_,
+      temp: '',
+      country: '',
+      feelsLike: this.feelsLike,
+      city: null,
+      currentUserLocation: this.currentUserLocation
     }
+  }
+
+  async componentDidMount() {
+    let weatherApi = `https://api.weatherapi.com/v1/current.json?key=c8ec5c78e09448f6bce75309220907&q=${this.state.currentUserLocation}'&aqi=no`
+    let data = await fetch(weatherApi)
+    let parsedData = await data.json()
+    let geoLication = 'https://geolocation-db.com/json/'
+    let locationData = await fetch(geoLication)
+    let parsedLocation = await locationData.json()
+    this.setState({
+      location: parsedData.location,
+      temp: `${parsedData.current.temp_c}°`,
+      country: parsedData.location.country,
+      city: parsedLocation.city,
+      feelsLike: `Feels like ${parsedData.current.feelslike_c}`,
+      currentUserLocation: parsedLocation.IPv4
+    })
   }
 
   changeCategory = async () => {
@@ -31,13 +56,23 @@ export default class App extends Component {
     console.log(this.state.category)
   }
 
+  // searchFunc = ()=>{
+  //   let userInputValue = document.getElementById("searchBox").value
+  //   console.log(userInputValue)
+  // }
 
-  render() {
+
+  render() {  
     return (
           
           <BrowserRouter>
-            <Navbar title="News-India" />
-            <Weather/>
+            <Navbar title="News-India"/>
+            <Weather 
+              temp={this.state.temp} 
+              city={this.state.city} 
+              country={this.state.country} 
+              feelsLike={this.state.feelsLike}
+            />
             {/* <Crowsel /> */}
             <Routes>
               <Route path="/" element={<News key={"general"} category={"general"} />}></Route>
@@ -49,18 +84,6 @@ export default class App extends Component {
             </Routes>
           </BrowserRouter>
     )
-
-    // return (
-    //   <div>
-    //     <Navbar title="News-India" category={this.changeCategory} />
-    //     <div className="container">
-    //       <Crowsel />
-    //       {/* key={} if components is not mounting after chnageing props, then add unique key  */}
-    //       <News key={this.state.key} category={this.state.category} />
-    //     </div>
-    //   </div>
-    // )
-
   }
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,51 +6,29 @@ import Menu_button from "./Menu_button.svg";
 export class Navbar extends Component {
 
     render() {
-        let { title } = this.props
+        let { title, searchFunction } = this.props
+
+        let openSearchBar = () => {
+            console.log("openSearchBar ")
+            document.getElementById("navMenus").style.display = "none"
+            document.getElementById("searchBox").style.display = "block"
+            document.getElementById("searchButton").style.display = "block"
+            document.getElementById("searchIcon").style.marginRight = "12px"
+        }
+        
         return (
-            // <nav className="navbar sticky-md-top navbar-expand-lg">
-            //     <div className="container-fluid">
-            //         <Link className="navbar-brand" to="/">{title}</Link>
-            //         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            //             <span className="navbar-toggler-icon"></span>
-            //         </button>
-            //         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            //             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            //                 <li className="nav-item d-flex">
-            //                     <NavLink className="nav-link" to="technology">Technology</NavLink>
-            //                 </li>
-            //                 <li className="nav-item d-flex">
-            //                     <NavLink className="nav-link" to="science">Science</NavLink>
-            //                 </li>
-            //                 <li className="nav-item d-flex">
-            //                     <NavLink className="nav-link" to="sports">Sports</NavLink>
-            //                 </li>
-            //                 <li className="nav-item d-flex">
-            //                     <NavLink className="nav-link" to="health">Health</NavLink>
-            //                 </li>
-            //                 <li className="nav-item d-flex">
-            //                     <NavLink className="nav-link" to="entertainment">Entertainment</NavLink>
-            //                 </li>
-            //             </ul>
-            //             <form className="d-flex" role="search">
-            //                 <input className="form-control me-2 px-4 rounded-5" type="search" placeholder="Search" aria-label="Search" />
-            //                 <button className="btn btn-outline-primary px-4 rounded-5" type="submit">Search</button>
-            //             </form>
-            //         </div>
-            //     </div>
-            // </nav>
             <div className="navContainer">
 
                 <div className="navbar">
                     <div className="leftDiv">
-                        <img src={Menu_button} alt="Search" onClick={() => { console.log("menuButton pressed") }} className="menuButton" />
+                        <img src={Menu_button} alt="Search" onClick={searchFunction} className="menuButton" />
                         <div className="seprater"></div>
                         <div className="logoNameContainer">
                             <Link id='nameOfApp' to="/">{title}</Link>
                         </div>
                     </div>
                     <div className="rightDiv">
-                        <div className="categoriesContainer">
+                        <div id='navMenus' className="categoriesContainer">
                             <NavLink className="nav-link" to="technology">Technology</NavLink>
                             <NavLink className="nav-link" to="science">Science</NavLink>
                             <NavLink className="nav-link" to="sports">Sports</NavLink>
@@ -59,9 +37,9 @@ export class Navbar extends Component {
                         </div>
                         <div className="seprater"></div>
                         <div className="searchBarContainer">
-                            <input id='searchBox' type="text" />
+                            <img id='searchIcon' src={Search_icon} alt="Search" onClick={openSearchBar} className="searchIcon" />
+                            <input id='searchBox' placeholder='eg: delhi, india, covid' type="text" />
                             <button id='searchButton'>Search</button>
-                            <img src={Search_icon} alt="Search" onClick={() => { console.log("helllow") }} className="searchIcon" />
                         </div>
                     </div>
                 </div>
